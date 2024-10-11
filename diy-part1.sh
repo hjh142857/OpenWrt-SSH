bd491df24b066496464d0e1dd757408f4473e085
LittlePri <23516992+hjh142857@users.noreply.github.com>
2024-10-11 15:48:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,19 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+#echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+
+# DIY package
+mkdir package/diy
+
+mkdir package/nas
+mkdir package/nas_luci
+git clone https://github.com/linkease/nas-packages package/nas
+git clone https://github.com/linkease/nas-packages-luci package/nas_luci
+mv package/nas/network/services/ddnsto package/diy/
+mv package/nas_luci/luci/luci-app-ddnsto package/diy/
+rm -rf package/nas
+rm -rf package/nas_luci
